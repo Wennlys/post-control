b7da3c4433aca98699ba052e061b7c4cfbf1127a
Wennlys Oliveira <wennlys@gmail.com>
2020-07-09 09:42:12 -0300

    Global configs

--- backend/src/Global/ExampleConfigs.php
+++ backend/src/Global/ExampleConfigs.php
@@ -4,10 +4,10 @@
  * SQL(POSTGRES) DATABASE
  * */
 define("SQL_DB", [
-    "HOST" => "172.17.0.2",
-    "USER" => "postgres",
-    "PASS" => "root",
-    "NAME" => "no-framework-cms"
+    "HOST" => "",
+    "USER" => "",
+    "PASS" => "",
+    "NAME" => ""
 ]);
 
 define("ENV_URI", "http://localhost:3333");
