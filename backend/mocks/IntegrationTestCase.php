55a65c823bab9f02d25ef6639e1a58c15c6af0a7
Wennlys Oliveira <wennlys@gmail.com>
2020-08-26 06:29:44 -0300

    IntegrationTestCase creation

---
+++ backend/mocks/IntegrationTestCase.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Mocks;
+
+use PHPUnit\Framework\TestCase;
+
+abstract class IntegrationTestCase extends TestCase
+{
+    protected string $actionClass;
+
+    protected function __construct(string $class)
+    {
+        parent::__construct();
+        $this->actionClass = $class;
+    }
+
+    protected function request(array $array): array
+    {
+        $json = json_encode($array);
+        $request = new ServerRequestMock();
+
+        $request->getBody()->write($json);
+        $response = (new $this->actionClass())($request);
+
+        return json_decode((string) $response->getBody(), true);
+    }
+}
