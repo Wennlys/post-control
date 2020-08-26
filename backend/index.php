8649f55478497eb96c1a38eaa4436fd8182e33e6
Wennlys Oliveira <wennlys@gmail.com>
2020-08-26 00:26:41 -0300

    routes methods simplification

--- backend/index.php
+++ backend/index.php
@@ -6,7 +6,6 @@ require_once dirname(__DIR__) . '/backend/vendor/autoload.php';
 
 use Dotenv\Dotenv;
 use League\Route\Router;
-use Source\Core\Connection;
 use Laminas\Diactoros\ResponseFactory;
 use League\Route\Strategy\JsonStrategy;
 use Laminas\Diactoros\ServerRequestFactory;
@@ -26,36 +25,34 @@ $responseFactory = new ResponseFactory();
 $strategy = new JsonStrategy($responseFactory);
 $router = new Router();
 
-$dbInstace = Connection::getInstance();
-
 $router->map(
     'GET',
     '/articles',
-    [new Source\App\Http\ArticleIndexController($dbInstace), 'index']
+    Source\App\Http\ArticleIndexAction::class
 );
 
 $router->map(
     'GET',
     '/articles/{id:number}',
-    [new Source\App\Http\ArticleShowController($dbInstace), 'show']
+    Source\App\Http\ArticleShowAction::class
 );
 
 $router->map(
     'POST',
     '/articles',
-    [new Source\App\Http\ArticleStoreController($dbInstace), 'store']
+    Source\App\Http\ArticleStoreAction::class
 );
 
 $router->map(
     'PUT',
     '/articles',
-    [new Source\App\Http\ArticleUpdateController($dbInstace), 'update']
+    Source\App\Http\ArticleUpdateAction::class
 );
 
 $router->map(
     'DELETE',
     '/articles/{id:number}',
-    [new Source\App\Http\ArticleDestroyController($dbInstace), 'destroy']
+    Source\App\Http\ArticleDestroyAction::class
 );
 
 $response = $router->dispatch($request);
